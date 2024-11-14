c667377e1cf8ff7554a8854a31470e1a89305561
Tate <149975543+Purpletiger808@users.noreply.github.com>
2024-11-14 15:55:54 -0600

    Update Main.java
    updated for 11/14 session

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
         Card[] deckOf52 = new Card[52];
         Card[] deckOf104 = new Card[104];
         fillStandardDeck(deckOf52);
         fillNonStandardDeck(deckOf104, 2);
-        for(Card c : deckOf104){
-            System.out.println(c);
-        }
+
+        Player user = new Player(true);
+        Player cpu = new Player(false);
+        fillHandOnStart(user, deckOf52);
+        fillHandOnStart(cpu, deckOf52);
+        System.out.println(user);
+        System.out.println(cpu);
 
     }
 
@@ -25,7 +32,7 @@ public class Main {
         int k = 0;
         while(k != numDecks){
             int j = 1;
-            for(int i = 0; i < deck.length; i+=4){
+            for(int i = 0; i < deck.length/numDecks; i+=4){
                 deck[i + (52*k)] = new Card("clubs", j);
                 deck[i+1 + (52*k)] = new Card("diamonds", j);
                 deck[i+2 + (52*k)] = new Card("hearts", j);
@@ -37,6 +44,12 @@ public class Main {
     }
 
     public static Card draw(Card[] deck){
-        return deck[(int)(Math.floor(Math.random() * (52)) + 1)];
+        return deck[(int)(Math.floor(Math.random() * (52)))];
+    }
+
+    public static void fillHandOnStart(Player p, Card[] deck){
+        for(int i = 0; i < 7; i++){
+            p.addCardToHand(draw(deck));
+        }
     }
 }
